86d66b829341ddd5f7349130f964cc4ab79b25a0
Dean Radcliffe <deanmisc@yahoo.com>
2015-10-08 17:02:12 -0500

    ES6ify

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
 Package.describe({
   name: 'okgrow:promise',
-  version: '0.9.6',
+  version: '0.10.1',
   summary: 'Utilities for Promise-based wrappers, method calls, helpers and HTTP in Meteor',
   git: 'https://github.com/okgrow/meteor-promise',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
+  api.versionsFrom('1.2.0');
   api.use(['ddp', 'http', 'tracker', 'underscore']);
   //api.use('ecmascript');
   api.use('promise@0.4.1');
   api.imply('promise');
+  api.imply('ecmascript');
   api.addFiles('wrapPromise.js', 'client');
   api.addFiles('reactivePromise.js', 'client');
   api.export("ReactivePromise", 'client');
