4e3fea6f79f75e77b0f509a69de92b9c37e7e83e
Manuel Timita <timita@gmail.com>
2024-01-16 00:26:59 +0000

    Compatible with Meteor 3.0
    Meteor 3.0-beta.0 is out. This is to make it compatible with it

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'deanius:promise',
-  version: '3.1.5',
+  version: '3.1.6',
   summary: 'Utilities for Promise-based wrappers, method calls, helpers and HTTP in Meteor',
   git: 'https://github.com/deanius/meteor-promise',
   documentation: 'README.md'
@@ -11,7 +11,7 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom(['1.6.1', '2.3']);
+  api.versionsFrom(['1.6.1', '2.3', '3.0-beta.0']);
   api.use('ecmascript');
   api.use('promise');
   api.imply('promise');
